d35566cc19798ceead69b3d1bcb1d583eb85e919
aas86 <aas86@mail.ru>
2017-08-28 19:46:08 +0700

    Добавил условие пересечения и ещё один объект класса Range

--- Range/src/ru/academits/alaev/range/Range.java
+++ Range/src/ru/academits/alaev/range/Range.java
@@ -1,5 +1,7 @@
 package ru.academits.alaev.range;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 public class Range {
     private double from;
     private double to;
@@ -25,11 +27,20 @@ public class Range {
         return this.to - this.from;
     }
 
+    public double getNewSegment(){
+        //как то надо сюда 2 точки отправить и проверить условие
+        // пересечения point.from <= point2.end && point2.end >= point.from
+    }
+
     public static void main(String[] args) {
         double x = 2;
         Range point = new Range(10, 20);
+        Range point2 = new Range(5, 15);
+        System.out.println(point2.getFrom());
+        System.out.println(point.getFrom());
         System.out.println("Is point in range? - " + point.isInside(x));
         System.out.println("Length is - " + point.getLength());
+
     }
 }
 
