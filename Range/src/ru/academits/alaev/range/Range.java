364387fd9f8fd505f5f02cdce4f0ceafad26a6eb
aas86 <aas86@mail.ru>
2017-08-30 15:34:07 +0700

    1. Переделал случаи пересечения 2. Переделал вывод результата при возврате NULL

--- Range/src/ru/academits/alaev/range/Range.java
+++ Range/src/ru/academits/alaev/range/Range.java
@@ -40,10 +40,10 @@ public class Range {
         // а к полям объекта segment2 через объект, переданный в виде аргумента.
         // this можно не писать, но я оставил для понимания.
         // Т.о. получается, что через this я обращаюсь к полю объекта от которого вызвал метод.
-        if ((this.from < segment2.to) && (this.to > segment2.from)) {
+        if ((this.from < segment2.from) && (this.to > segment2.from) && (segment2.to > this.to)) {
+            return new Range(segment2.from, this.to);
+        } else if ((segment2.from < this.from) && (segment2.to > this.from) && (this.to > segment2.to)) {
             return new Range(this.from, segment2.to);
-        } else if ((segment2.from < this.to) && (segment2.to > this.from)) {
-            return new Range(segment2.from, to);
         } else if (this.from > segment2.from && this.to < segment2.to) {
             return new Range(this.from, this.to);
         } else if (segment2.from > this.from && segment2.to < this.to) {
