bbca51b5ee44dc65e34c7f6c7706eabd3a81ef7a
aas86 <aas86@mail.ru>
2017-08-30 10:59:32 +0700

    1. Переделал метод getIntersection 2. Переделал вызов метода 3. Переделал случаи пересечения 4. Добавил сеттер для поля to

--- Range/src/ru/academits/alaev/range/Main.java
+++ Range/src/ru/academits/alaev/range/Main.java
@@ -3,14 +3,16 @@ package ru.academits.alaev.range;
 public class Main {
     public static void main(String[] args) {
         double x = 2;
-        Range segment1 = new Range(10, 20);
-        System.out.printf("Segment1: %f %f", segment1.getFrom(), segment1.getTo());
+        Range segment1 = new Range(10, 20); // Создаю объект segment1
+        System.out.printf("Segment1: %f %f \n", segment1.getFrom(), segment1.getTo());
+        //System.out.println();
         System.out.println("Is point in range? - " + segment1.isInside(x));
         System.out.println("Length is - " + segment1.getLength());
-        Range segment2 = new Range(5, 15);
+        Range segment2 = new Range(5, 15); // Создаю объект segment2
         System.out.printf("Segment2: %f %f", segment2.getFrom(), segment2.getTo());
         System.out.println();
-        System.out.println(getIntersection(segment1, segment2));
-        // Не понимаю от какого объекта вызывать метод getIntersection
+        Range result = segment1.getIntersection(segment2);
+        System.out.printf("Result segment: %f %f", result.getFrom(), result.getTo());
+
     }
 }
--- Range/src/ru/academits/alaev/range/Range.java
+++ Range/src/ru/academits/alaev/range/Range.java
@@ -21,6 +21,10 @@ public class Range {
         this.from = from;
     }
 
+    public void setTo(double to) {
+        this.to = to;
+    }
+
 
     public boolean isInside(double x) {
         return x >= from && x <= to;
@@ -30,15 +34,20 @@ public class Range {
         return this.to - this.from;
     }
 
-    public Range getIntersection(Range segment1, Range segment2) {
-        if (segment1.from < segment2.to) {
-            return new Range(segment1.from, segment2.to);
-        } else if (segment2.from < segment1.to) {
-            return new Range(segment2.from, segment1.to);
-        } else if (segment1.from > segment2.from && segment1.to < segment2.to) {
-            return segment1;
-        } else if(segment2.from > segment1.from && segment2.to < segment1.to){
-            return segment2;
+    public Range getIntersection(Range segment2) {
+        // Так как вызываю от segment1 (потому что метод не static),
+        // то к полям объекта segment1 можно обратиться через this
+        // а к полям объекта segment2 через объект, переданный в виде аргумента.
+        // this можно не писать, но я оставил для понимания.
+        // Т.о. получается, что через this я обращаюсь к полю объекта от которого вызвал метод.
+        if ((this.from < segment2.to) && (this.to > segment2.from)) {
+            return new Range(this.from, segment2.to);
+        } else if ((segment2.from < this.to) && (segment2.to > this.from)) {
+            return new Range(segment2.from, to);
+        } else if (this.from > segment2.from && this.to < segment2.to) {
+            return new Range(this.from, this.to);
+        } else if (segment2.from > this.from && segment2.to < this.to) {
+            return new Range(segment2.from, segment2.to);
         } else {
             return null;
         }
