006bc263c42fd3816e5052511a2209314baf9598
aas86 <aas86@mail.ru>
2017-08-31 17:35:57 +0700

    Переделал getSum() метод на getConcatenation()

--- Range/src/ru/academits/alaev/range/Main.java
+++ Range/src/ru/academits/alaev/range/Main.java
@@ -3,20 +3,22 @@ package ru.academits.alaev.range;
 public class Main {
     public static void main(String[] args) {
         double x = 2;
-        Range segment1 = new Range(1, 5); // Создаю объект segment1
+        Range segment1 = new Range(10, 20); // Создаю объект segment1
         System.out.printf("Segment1: %f %f \n", segment1.getFrom(), segment1.getTo());
         System.out.println("Is point in range? - " + segment1.isInside(x));
         System.out.println("Length is - " + segment1.getLength());
-        Range segment2 = new Range(10, 20); // Создаю объект segment2
+        Range segment2 = new Range(5, 15); // Создаю объект segment2
         System.out.printf("Segment2: %f %f", segment2.getFrom(), segment2.getTo());
         System.out.println();
         Range result = segment1.getIntersection(segment2);
         if (result == null) {
             System.out.println("No intersection!");
         } else {
-            System.out.printf("Result segment: %f %f", result.getFrom(), result.getTo());
+            System.out.printf("Result segment: %f %f \n", result.getFrom(), result.getTo());
         }
-        int i = 0; // переменная для цикла, не могу создать её в классе, поэтому передаю
-        Range[] result2 = segment1.getSum(segment2, i);
+        Range[] result2 = segment1.getConcatenation(segment2);
+        for (int i = 0; i < result2.length; ++i) {
+            System.out.printf("%f %f ", result2[i].getFrom(), result2[i].getTo());
+        }
     }
 }
--- Range/src/ru/academits/alaev/range/Range.java
+++ Range/src/ru/academits/alaev/range/Range.java
@@ -53,29 +53,22 @@ public class Range {
         }
     }
 
-    public Range[] getSum(Range segment2, int i) {
+    public Range[] getConcatenation(Range segment2) {
         if ((this.to <= segment2.from) || (segment2.to <= this.from)) {
-            // В этом случае отрезки не пересекаются тогда сумма будет состоять из двух
-            // отрезков, следовательно я возвращаю массив из двух новых объектов класса Range. А как их заполнить
-            // нужными значениями? Циклом не получается, тогда заполняю вручную поэлементно.
-            // Но что-то подсказывает, что не правильно.
-            // и что - то не могу сообразить, как напечатать потом из main результат.
-
             Range[] result = new Range[2];
-            result[0].setFrom(this.from);
-            result[0].setTo(this.to);
-            result[1].setFrom(segment2.from);
-            result[1].setTo(segment2.to);
+            // Т.к. 2 отрезка, то нужно вернуть массив из 2ух новых объектов
+            // result - reference тип, а значит нужно указать на что ему ссылаться,
+            // а ему нужно ссылаться на новый объект, который мы создаём конструктором
+            result[0] = new Range(this.from, this.to);
+            result[1] = new Range(segment2.from, segment2.to);
             return result; // Возвращаю массив result
         } else if (this.to < segment2.to) {
             Range[] result = new Range[1];
-            result[0].setFrom(this.from);
-            result[0].setTo(segment2.to);
+            result[0] = new Range(this.from, segment2.to);
             return result;
         } else {
             Range[] result = new Range[1];
-            result[0].setFrom(segment2.from);
-            result[0].setTo(this.to);
+            result[0] = new Range(segment2.from, this.to);
             return result;
         }
     }
