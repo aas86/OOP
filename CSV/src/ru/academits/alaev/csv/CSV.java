91e1fc96ede8e4c9711e6095d68890b24aad6937
aas86 <aas86@mail.ru>
2017-09-07 15:30:08 +0700

    Не понятно, как обрабатывать перевод и ",

--- CSV/src/ru/academits/alaev/csv/CSV.java
+++ CSV/src/ru/academits/alaev/csv/CSV.java
@@ -8,27 +8,42 @@ import java.util.Scanner;
 public class CSV {
     public static void main(String[] args) throws FileNotFoundException {
         try (PrintWriter writer = new PrintWriter("output.html");
-             Scanner scanner = new Scanner(new FileInputStream("src\\ru\\academits\\alaev\\csv\\CSV.txt"))) {
+             Scanner scanner = new Scanner(new FileInputStream("CSV\\src\\ru\\academits\\alaev\\csv\\CSV.txt"))) {
             writer.println("<!doctype HTML public\"-//W3C//Dtd HTML 4.0 Frameset // EN\"");
             writer.println("<html>");
             writer.println("<head>");
             writer.println("</head>");
             writer.println("<body>");
-            char lineSeparator = '\n';
             while (scanner.hasNextLine()) {
                 writer.print("<tr><td>");
                 String string = scanner.nextLine();
                 for (int i = 0; i < string.length(); ++i) {
                     char c = string.charAt(i);
-                    if (c == ',') {
-                        writer.print("</td><td>");
-                    } else if (string.charAt(i) == '"') {
-                        writer.print("</br>");
+                    if (string.charAt(i) == '"') {
+                        int j = i + 1;
+                        if (string.charAt(j) == '"') {
+                            writer.print(string.charAt(j));
+                            i = j;
+                        } else {
+                            while (string.charAt(j) != '"' && string.charAt(j + 1) != ',') {
+                                if (string.charAt(j) == ',') {
+                                    writer.print(string.charAt(j));
+                                    j++;
+                                    i = j;
+                                } else {
+                                    writer.print(string.charAt(j));
+                                    j++;
+                                    i = j;
+                                }
+                            }
+                        }
+                    } else if (string.charAt(i) == ',') {
+                        writer.print("</td> <td>");
                     } else {
-                        writer.print(c);
+                        writer.print(string.charAt(i));
                     }
                 }
-                writer.print("</tr><br/>");
+                writer.print("</td></tr><br/>");
             }
             writer.println("</body>");
             writer.println("</html>");
