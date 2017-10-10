bf7447b95925d95608920612fff239c4f823f31e
aas86 <aas86@mail.ru>
2017-10-10 10:28:41 +0700

    Попробовал сделать конструкторы a,b,c из задания

--- Vectors/src/ru/academits/alaev/Main.java
+++ Vectors/src/ru/academits/alaev/Main.java
@@ -6,5 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         Vector vector = new Vector(4);
+
+        Vector vector1 = new Vector(vector);
+
+        double[] a = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Vector vector2 = new Vector(a);
+
+        double[] b = new double[]{1, 2, 3, 4, 5};
+        Vector vector3 = new Vector(10, b);
     }
 }
+
--- Vectors/src/ru/academits/alaev/vector/Vector.java
+++ Vectors/src/ru/academits/alaev/vector/Vector.java
@@ -3,20 +3,28 @@ package ru.academits.alaev.vector;
 public class Vector {
     private int n;      //размерность вектора
     private double[] vector;
+    private Vector clone;
+    private double[] array;
 
-    public Vector(int n) { //Конструктор вектора принимает число n – размерность вектора.
+
+
+    public Vector(int n) { // Конструктор вектора принимает число n – размерность вектора.
         this.n = n;
-        this.vector = new double[n];
+        this.vector = new double[n]; // По умолчанию создаётся массив 0 размерности n
     }
 
-    public double[] getVector(){
-        return this.vector;
-    }
-    public int getN() {
-        return n;
+    public Vector(Vector vector) { // Конструктор копирования, т.е. по сути ссылочной переменной clone
+                                   // присваивается ссылочная переменная vector. И теперь и vector, и clone
+                                   // указывают на один и тот же массив из нулей.
+        this.clone = vector;
     }
 
-    public void setN(int n) {
-        this.n = n;
+    public Vector(double[] array) { // передаю в конструктор массив и его же записываю в новый массив
+        this.array = array;
     }
+
+    public Vector(int n, double[] array) {
+
+    }
+
 }
