6064eea64ffb3308373bdac033035fb9cf4c1201
aas86 <aas86@mail.ru>
2017-10-10 15:59:04 +0700

    1. Переделал конструкторы a, b, c. 2. Сделал метод getSize (длина вектора)

--- Vectors/src/ru/academits/alaev/Main.java
+++ Vectors/src/ru/academits/alaev/Main.java
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Vector vector = new Vector(4);
 
-        Vector vector1 = new Vector(vector);
+        Vector vector1 = new Vector(vector);  // Вызов конструктора копирования
 
         double[] a = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         Vector vector2 = new Vector(a);
+        System.out.println(vector2.getSize(vector2));
 
         double[] b = new double[]{1, 2, 3, 4, 5};
         Vector vector3 = new Vector(10, b);
--- Vectors/src/ru/academits/alaev/vector/Vector.java
+++ Vectors/src/ru/academits/alaev/vector/Vector.java
@@ -1,30 +1,36 @@
 package ru.academits.alaev.vector;
 
 public class Vector {
-    private int n;      //размерность вектора
-    private double[] vector;
-    private Vector clone;
-    private double[] array;
 
+    private double[] vector;
 
 
     public Vector(int n) { // Конструктор вектора принимает число n – размерность вектора.
-        this.n = n;
         this.vector = new double[n]; // По умолчанию создаётся массив 0 размерности n
     }
 
-    public Vector(Vector vector) { // Конструктор копирования, т.е. по сути ссылочной переменной clone
-                                   // присваивается ссылочная переменная vector. И теперь и vector, и clone
-                                   // указывают на один и тот же массив из нулей.
-        this.clone = vector;
+    public Vector(Vector vector) {   // Конструктор копирования принимает объект типа Vector
+        this.vector = vector.getVector();
+
     }
 
     public Vector(double[] array) { // передаю в конструктор массив и его же записываю в новый массив
-        this.array = array;
+        setVector(array);
     }
 
     public Vector(int n, double[] array) {
 
     }
 
+    private double[] getVector() {
+        return vector;
+    }
+
+    private void setVector(double[] vector) {
+        this.vector = vector;
+    }
+
+    public int getSize(Vector vector) {
+        return vector.getVector().length;
+    }
 }
