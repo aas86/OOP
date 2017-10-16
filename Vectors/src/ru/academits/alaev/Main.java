923879648ca9d6adf3bfe05076dc16dbfa9f930e
aas86 <aas86@mail.ru>
2017-10-16 16:57:58 +0700

    1. Переделал конструкторы с System.arraycopy на copyOf(); 2. Переделал методы нестатик методы getSum() и getDifference(); 3. Добавил методы умножение на скаляр; 4. Добавил метод получения длины; 5. Добавил 3 статик метода.

--- Vectors/src/ru/academits/alaev/Main.java
+++ Vectors/src/ru/academits/alaev/Main.java
@@ -10,24 +10,63 @@ public class Main {
 
         double[] array1 = new double[]{1, 2, 3, 4, 5, 6, 7};
         Vector vectorC = new Vector(array1);         // Вызов конструктора с массивом в качестве аргумента
+        System.out.println("Вызов конструктора с массивом в качестве аргумента");
+        System.out.println(vectorC);
         Vector vectorB = new Vector(vectorC);        // Вызов конструктора копирования
+        System.out.println("Копируем предыдущий вектор");
+        System.out.println(vectorB);
 
         double[] array2 = new double[]{1, 2, 3, 4, 5};
         Vector vectorD = new Vector(10, array2);
 
 
-  // Проверка методов getSum() и getDifference
+        // Проверка методов getSum() и getDifference
         System.out.println(vectorD.getSize());
         System.out.println(vectorA.getSize());
 
-        double[] array3 = new double[]{1, 2, 3};
-        double[] array4 = new double[]{100, 200, 300};
+        double[] array3 = new double[]{1, 2, 3, 4, 5, 6};
+        double[] array4 = new double[]{1, 2, 3};
 
         Vector a = new Vector(array3);
         Vector b = new Vector(array4);
+        System.out.println(a);
+        System.out.println(b);
 
         Vector c = a.getSum(b);
-        Vector d = a.getDifference(b);
+        System.out.println("Cумма векторов ");
+        System.out.println(c);
+
+        double[] array5 = new double[]{1, 2, 3, 4, 5, 6};
+        double[] array6 = new double[]{1, 2, 3};
+        Vector e = new Vector(array5);
+        Vector f = new Vector(array6);
+        Vector d = f.getDifference(e);
+
+        System.out.println("Разность векторов ");
+        System.out.println(d);
+
+        System.out.println("Умножение вектора на скаляр ");
+        System.out.println(f);
+        f.scalarMultiplication(5);
+        System.out.println(f);
+
+        double[] array7 = new double[]{1, 2, 3};
+        Vector g = new Vector(array7);
+        System.out.println("Длина вектора " + g.getLength());
+
+
+        double[] array8 = new double[]{1, 2, 3};
+        double[] array9 = new double[]{1, 2, 3, 100, 200};
+        Vector x = new Vector(array8);
+        Vector y = new Vector(array9);
+        System.out.println(x);
+        System.out.println(y);
+        Vector z1 = Vector.getSum(x, y);
+        System.out.println("Сумма векторов(static метод) " + z1);
+        Vector z2 = Vector.getDifference(x, y);
+        System.out.println("Разность векторов(static метод) " + z2);
+        double scalarProduct = Vector.scalarProduct(x, y);
+        System.out.println("Скалярное произведение векторов = " + scalarProduct);
     }
 }
 
--- Vectors/src/ru/academits/alaev/vector/Vector.java
+++ Vectors/src/ru/academits/alaev/vector/Vector.java
@@ -1,43 +1,41 @@
 package ru.academits.alaev.vector;
 
+import java.util.Arrays;
+
 public class Vector {
 
     private double[] vector;
 
 
-    public Vector(int n) { // Конструктор вектора принимает число n – размерность вектора.
+    public Vector(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("value of 'n' is negative: n = " + n + "");
         }
         this.vector = new double[n]; // По умолчанию создаётся массив 0 размерности n
     }
 
-    public Vector(Vector vector) {   // Конструктор копирования принимает объект типа Vector
-        // Т.к. поле vector не определено (NULL), то нельзя взять его длину, но длину можно взять из принятого объекта,
-        // который является тоже массивом с полем length. Эта длина будет 5ым аргументом метода arraycopy() Для того,
-        // чтобы получить эту длину прохожусь по всему массиву и считаю кол-во элементов.
-        this.vector = new double[vector.vector.length];
-        System.arraycopy(vector.vector, 0, this.vector, 0, vector.vector.length);
-        // из поля принятого вектора положить в поле vector. Именно не ссылку, а поэлементно. (arraycopy() заменяет
-        // проход по массиву циклом)
+    public Vector(Vector vector) {
+        this.vector = Arrays.copyOf(vector.vector, vector.vector.length);
     }
 
-    public Vector(double[] array) { // передаю в конструктор массив и его же записываю в новый массив
-        this.vector = new double[array.length];
-        System.arraycopy(array, 0, this.vector, 0, array.length);
+    public Vector(double[] array) {
+        this.vector = Arrays.copyOf(array, array.length);
     }
 
     public Vector(int n, double[] array) {
-        this.vector = new double[n];
-        System.arraycopy(array, 0, this.vector, 0, array.length);
+        this.vector = Arrays.copyOf(array, n);
     }
 
-    public double getVectorElement(int i) {
-        return this.vector[i];
+    public double getElement(int i) {
+        if (i > this.vector.length) {
+            return 0;
+        } else {
+            return this.vector[i];
+        }
     }
 
 
-    public void setVectorElement(int i, double element) {
+    public void setElement(int i, double element) {
         this.vector[i] = element;
     }
 
@@ -45,55 +43,101 @@ public class Vector {
         return this.vector.length;
     }
 
-    public String toString() {
-        char[] ch = new char[vector.length];
-        for (int i = 0; i < this.vector.length; ++i) {
-            ch[i] = (char) this.vector[i];
-        }
-        return new String(ch);
-    }
-
     public Vector getSum(Vector vector) {
         if (this.vector.length > vector.vector.length) {
-            Vector newVector = new Vector(this.vector);
             for (int i = 0; i < vector.vector.length; ++i) {
-                newVector.vector[i] = this.vector[i] + vector.vector[i];
+                this.vector[i] = this.vector[i] + vector.vector[i];
             }
-            return newVector;
-        } else if (this.vector.length < vector.vector.length) {
-            Vector newVector = new Vector(vector.vector);
-            for (int i = 0; i < this.vector.length; ++i) {
-                newVector.vector[i] = this.vector[i] + vector.vector[i];
-            }
-            return newVector;
+            return this;
         } else {
-            Vector newVector = new Vector(this.vector.length);
             for (int i = 0; i < this.vector.length; ++i) {
-                newVector.vector[i] = this.vector[i] + vector.vector[i];
+                vector.vector[i] = this.vector[i] + vector.vector[i];
             }
-            return newVector;
+            return vector;
         }
     }
 
     public Vector getDifference(Vector vector) {
         if (this.vector.length < vector.vector.length) {
-            Vector newVector = new Vector(vector.vector.length, this.vector);
-            for (int i = 0; i < newVector.vector.length; ++i) {
-                newVector.vector[i] = newVector.vector[i] - vector.vector[i];
+            /*double[] temp = new double[this.vector.length];
+            System.arraycopy(this.vector, 0, temp, 0, this.vector.length);
+            this.vector = new double[vector.vector.length];
+            System.arraycopy(temp, 0, this.vector, 0, temp.length);*/
+            this.vector = Arrays.copyOf(this.vector, vector.vector.length);
+            for (int i = 0; i < vector.vector.length; ++i) {
+                this.vector[i] = this.vector[i] - vector.vector[i];
             }
-            return newVector;
-        } else if (this.vector.length > vector.vector.length) {
-            Vector newVector = new Vector(this.vector.length, vector.vector);
-            for (int i = 0; i < newVector.vector.length; ++i) {
-                newVector.vector[i] = this.vector[i] - newVector.vector[i];
+            return this;
+        } else {
+            for (int i = 0; i < vector.vector.length; ++i) {
+                this.vector[i] = this.vector[i] - vector.vector[i];
+            }
+            return this;
+        }
+    }
+
+    public void scalarMultiplication(double scalar) {
+        for (int i = 0; i < this.vector.length; ++i) {
+            this.vector[i] = this.vector[i] * scalar;
+        }
+    }
+
+    public double getLength() {
+        double sum = 0;
+        for (double e : this.vector) {
+            sum += Math.pow(e, 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+    public String toString() {
+        return Arrays.toString(vector);
+    }
+
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        if (vector1.getSize() > vector2.getSize()) {
+            Vector newVector = new Vector(vector1.getSize(), vector1.vector);
+            for (int i = 0; i < vector2.getSize(); ++i) {
+                newVector.setElement(i, vector1.getElement(i) + vector2.getElement(i));
             }
             return newVector;
         } else {
-            Vector newVector = new Vector(this.vector.length);
-            for (int i = 0; i < this.vector.length; ++i) {
-                newVector.vector[i] = this.vector[i] - vector.vector[i];
+            Vector newVector = new Vector(vector2);
+            for (int i = 0; i < vector1.getSize(); ++i) {
+                newVector.setElement(i, vector1.getElement(i) + vector2.getElement(i));
             }
             return newVector;
+        }
+    }
+
+    public static Vector getDifference(Vector vector1, Vector vector2) {
+        if (vector1.getSize() < vector2.getSize()) {
+            Vector newVector = new Vector(vector2.getSize(), vector1.vector);
+            for (int i = 0; i < newVector.getSize(); ++i) {
+                newVector.setElement(i, newVector.getElement(i) - vector2.getElement(i));
+            }
+            return newVector;
+        } else {
+            Vector newVector = new Vector(vector1.getSize(), vector1.vector);
+            for (int i = 0; i < vector2.getSize(); ++i) {
+                newVector.setElement(i, newVector.getElement(i) - vector2.getElement(i));
+            }
+            return newVector;
+        }
+    }
+
+    public static double scalarProduct(Vector vector1, Vector vector2) {
+        double scalarProduct = 0;
+        if (vector1.getSize() < vector2.getSize()) {
+            for (int i = 0; i < vector1.getSize(); ++i) {
+                scalarProduct += vector1.getElement(i) * vector2.getElement(i);
+            }
+            return scalarProduct;
+        } else {
+            for (int i = 0; i < vector2.getSize(); ++i) {
+                scalarProduct += vector1.getElement(i) * vector2.getElement(i);
+            }
+            return scalarProduct;
         }
     }
 }
