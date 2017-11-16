735c1c3d7c258542d109b7355fa64ab9457a09fe
aas86 <aas86@mail.ru>
2017-11-16 14:46:12 +0700

    SinglyLinkedList

--- SinglyLinkedList/src/ru/academits/alaev/ru/academits/alaev/main/Main.java
+++ SinglyLinkedList/src/ru/academits/alaev/ru/academits/alaev/main/Main.java
@@ -12,19 +12,85 @@ public class Main {
         //Тут создаём объекты списка
         ListItem<Integer> object0 = new ListItem<>(777);
         ListItem<Integer> object1 = new ListItem<>(555);
-
+        ListItem<Integer> object2 = new ListItem<>(111);
         //вставка в начало
-        System.out.println(list.getSize());
+        //   System.out.println(list.getSize());
         list.beginInsert(object0);
         System.out.println(list.getFirstElement());
         System.out.println(list.getSize());
         list.beginInsert(object1);
         System.out.println(list.getFirstElement());
-        list.beginInsert(new ListItem<>(888));
-        list.beginInsert(new ListItem<>(111));
+        //  list.beginInsert(new ListItem<>(888));
+        //list.beginInsert(new ListItem<>(111));
+        list.beginInsert(object2);
         System.out.println(list.getFirstElement());
-        System.out.println(list.getElement(1));
+        System.out.printf("%d %d %d ", list.getElement(0), list.getElement(1), list.getElement(2));
+        //   System.out.println(list.getElement(1));
+        //   System.out.println(list.getElement(2));
+        System.out.println();
         System.out.println(list.getSize());
+        //  list.insertElement(2, new ListItem<>(7));
+        for (int i = 0; i < list.getSize(); ++i) {
+            System.out.printf("%d ", list.getElement(i));
+        }
+      /*  System.out.println();
+        System.out.println("Проверка метода удаления элемента по индексу");
+        System.out.println("Удалённый элемент : " + list.delete(2));
+        for (int i = 0; i < list.getSize(); ++i){
+            System.out.printf("%d " , list.getElement(i));
+        }
+        System.out.println();
+        System.out.println("Проверка метода удаления первого элемента:");
+        list.deleteFirst();
+        for (int i = 0; i < list.getSize(); ++i){
+            System.out.printf("%d " , list.getElement(i));
+        }*/
 
+        System.out.println();
+        System.out.println("Проверка метода удаления элемента по значению:");
+        ListItem<Integer> object3 = new ListItem<>(111);
+        list.delete(object3);
+        for (int i = 0; i < list.getSize(); ++i) {
+            System.out.printf("%d ", list.getElement(i));
+        }
+        System.out.println();
+        System.out.println("Проверка метода вставка узла после указанного :");
+        ListItem<Integer> object4 = new ListItem<>(444);
+        list.insertAfter(1, object4);
+        for (int i = 0; i < list.getSize(); ++i) {
+            System.out.printf("%d ", list.getElement(i));
+        }
+        System.out.println("Проверка метода вставка узла после указанного :");
+        ListItem<Integer> object5 = new ListItem<>(222);
+        list.insertAfter(1, object5);
+        for (int i = 0; i < list.getSize(); ++i) {
+            System.out.printf("%d ", list.getElement(i));
+        }
+
+
+        list.deleteAfter(1);
+        System.out.println();
+        System.out.println("Проверка метода удаления узла после указанного :");
+        for (int i = 0; i < list.getSize(); ++i) {
+            System.out.printf("%d ", list.getElement(i));
+        }
+
+        System.out.println();
+   /*     System.out.println("Проверка метода изменения значения по индексу (выдает старое значение) :");
+        ListItem<Integer> object6 = new ListItem<>(0);
+        System.out.println("Старое значение элемента: " + list.setElement(1, object6));
+        for (int i = 0; i < list.getSize(); ++i){
+            System.out.printf("%d " , list.getElement(i));
+        }*/
+        ListItem<Integer> object6 = new ListItem<>(0);
+        list.beginInsert(object6);
+        for (int i = 0; i < list.getSize(); ++i) {
+            System.out.printf("%d ", list.getElement(i));
+        }
+        System.out.println("Разворот ");
+        list.reverse();
+        for (int i = 0; i < list.getSize(); ++i) {
+            System.out.printf("%d ", list.getElement(i));
+        }
     }
 }
--- SinglyLinkedList/src/ru/academits/alaev/singlylinkedlist/SinglyLinkedList.java
+++ SinglyLinkedList/src/ru/academits/alaev/singlylinkedlist/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package ru.academits.alaev.singlylinkedlist;
 
+import java.util.List;
+
 public class SinglyLinkedList<T> {
     private ListItem<T> head;
     private int size;
@@ -17,10 +19,66 @@ public class SinglyLinkedList<T> {
         this.head = head;
     }
 
+    // 1. Получение размера списка
     public int getSize() {
         return size;
     }
 
+    // 2. Получение первого узла
+    public T getFirstElement() {
+        return head.getData();
+    }
+
+    // 3. Изменение значения по индексу пусть выдает старое значение.
+    public T setElement(int index, ListItem<T> element) {
+        if (index > size - 1 || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        int i = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (i == index) {
+                ListItem<T> temp = new ListItem<T>();
+                temp.setData(p.getData());
+                p.setData(element.getData());
+                return temp.getData();
+            }
+            i++;
+        }
+        return null;
+    }
+
+    // 4. Получение узла по индексу
+    public T getElement(int index) {
+        if (index > size - 1 || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        int i = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (i == index) {
+                return p.getData();
+            }
+            i++;
+        }
+        return null;
+    }
+
+    // 5. Удаление элемента по индексу, пусть выдает значение элемента(Удалённого?)
+    public T delete(int index) {
+        int i = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (i == index - 1) {
+                ListItem<T> q = p.getNext();
+                p.setNext(q.getNext());
+                this.size--;
+                return q.getData();
+            }
+            i++;
+        }
+        return null;
+    }
+
+
+    // 6. Вставка элемента в начало
     public void beginInsert(ListItem<T> o) {
         ListItem<T> temp = head;
         this.head = o;
@@ -28,28 +86,93 @@ public class SinglyLinkedList<T> {
         this.size++;
     }
 
-    public T getFirstElement() {
-        return head.getData();
+    // 7. вставка элемента по индексу
+    public void insertElement(int index, ListItem<T> element) {
+        int i = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (i == index - 1) {
+                element.setNext(p.getNext());
+                p.setNext(element);
+
+                this.size++;
+            }
+            i++;
+        }
     }
 
-    public void deleteFirst() {
+    // 8. Удаление узла по значению
+    public void delete(ListItem<T> element) {
+        int i = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (p == head) {
+                if (p.getData().equals(element.getData())) {
+                    deleteFirst();
+                    break;
+                }
+            }
+            ListItem<T> a = p.getNext();
+            if (a.getData().equals(element.getData())) {
+                p.setNext(a.getNext());
+                size--;
+                break;
+            }
+
+        }
+    }
+
+    // 9. Удаление первого элемента, пусть выдает значение элемента
+    public T deleteFirst() {
         this.head = this.head.getNext();
         size--;
+        return this.head.getData();
     }
 
-    public T getElement(int index) {
-        if (index > size - 1){
-            throw new IndexOutOfBoundsException();
+    // 10 Вставка и удаление узла после указанного узла
+    // 10.1 Вставка
+    public void insertAfter(int index, ListItem<T> element) {
+        if (index > size - 1 || index < 0) {
+            throw new NullPointerException("Нет такого индекса");
         }
         int i = 0;
-        T element = null;
         for (ListItem<T> p = head; p != null; p = p.getNext()) {
             if (i == index) {
-                element =  p.getData();
+                element.setNext(p.getNext());
+                p.setNext(element);
+                this.size++;
                 break;
             }
             i++;
         }
-        return element;
+    }
+
+    //// 10.2 Удаление
+    public void deleteAfter(int index) {
+        if (index >= size - 1 || index < 0) {
+            throw new NullPointerException("Нет элемента за указанным!");
+        }
+        int i = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (i == index) {
+                ListItem<T> q = p.getNext();
+                p.setNext(q.getNext());
+                this.size--;
+            }
+            i++;
+        }
+    }
+
+    // 11. Разворот
+    public void reverse() {
+        ListItem<T> temp = new ListItem<T>();
+        for (ListItem<T> p = head; p != null; p = temp.getNext()) {
+            while (p.getNext() != null) {
+                temp.setNext(p.getNext().getNext());
+                p.getNext().setNext(p);
+                p.setNext(temp.getNext());
+            }
+        }
+
     }
 }
+
+
