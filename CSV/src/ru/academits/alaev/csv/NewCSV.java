bdb2a053c8bbc247f3f97b7c969a23df3c2b0ad3
aas86 <aas86@mail.ru>
2017-12-22 11:02:02 +0700

    CSV  1. программа берет пути к файлам из аргументов программы;  2. doctype имправил на HTML 5;  3. Убрал пробелы вокруг = в атрибутах;  4. isInside -> insideQuotes;  5. Поправил тег таблицы и тег <br/>;  6. Убрал warning, изменил кодировку;  7. Обработал <,>,&.

--- CSV/src/ru/academits/alaev/csv/NewCSV.java
+++ CSV/src/ru/academits/alaev/csv/NewCSV.java
@@ -1,7 +1,5 @@
 package ru.academits.alaev.csv;
 
-import sun.plugin.dom.css.CSSValue;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -9,57 +7,75 @@ import java.util.Scanner;
 
 public class NewCSV {
     public static void main(String[] args) throws FileNotFoundException {
-        try (PrintWriter writer = new PrintWriter("CSV\\src\\ru\\academits\\alaev\\csv\\output.html");
-             Scanner scanner = new Scanner(new FileInputStream("CSV\\src\\ru\\academits\\alaev\\csv\\input_task.txt"))) {
-            writer.println("<!doctype HTML public\"-//W3C//Dtd HTML 4.0 Frameset // EN\"");
+        try (PrintWriter writer = new PrintWriter(args[1]);
+             Scanner scanner = new Scanner(new FileInputStream(args[0]))) {
+            writer.println("<!DOCTYPE html>");
             writer.println("<html>");
             writer.println("<head>");
-            writer.print("<title>");
-            writer.print("Задача CSV");
-            writer.println("</title>");
+            writer.println("<meta charset=\"utf-8\">");
+            writer.print("<title> Задача CSV </title>");
             writer.println("</head>");
             writer.println("<body>");
-            writer.println("<table border=\"1\" cellspacing = \"0\" align = \"center\"");
-            boolean isInside = false;
+            writer.println("<table border=\"1\" cellspacing=\"0\" align=\"center\"");
+            boolean insideQuotes;
             while (scanner.hasNextLine()) {
                 writer.print("<tr>");
                 writer.print("<td>");
-                String string = scanner.nextLine(); // получили первую строку
-            //    System.out.println(string.length());
-                for (int i = 0; i < string.length(); i++) {//цикл прохода по строке
+                String string = scanner.nextLine();
+                for (int i = 0; i < string.length(); i++) {
                     char symbol = string.charAt(i);
-                    if (symbol == '"') {// в строке есть ',' или перевод строки, или просто ", или пустая ячейка в кавычках
-                        isInside = true;
+                    if (symbol == '"') {
+                        insideQuotes = true;
                         i++;
-                        while (isInside) { // пока внутри ячейки, заключенной в кавычки, то если встречаются 2 кавычки подряд, то это обычная кавычка.
-                            // Если встречается 1 кавычка - то это закрывающая кавычка для ячейки.
-                            if (string.charAt(i) == '"' && string.charAt(i + 1) == '"'){
-                       //         System.out.print(string.charAt(i));
+                        while (insideQuotes) {
+                            if (string.charAt(i) == '"' && string.charAt(i + 1) == '"') {
                                 writer.print(string.charAt(i + 1));
                                 i = i + 2;
                             } else {
                                 while (string.charAt(i) != '"') {
-                               //     System.out.print(string.charAt(i));
-                                    writer.print(string.charAt(i));
+                                    switch (string.charAt(i)) {
+                                        case '<':
+                                            writer.print("&lt");
+                                            break;
+                                        case '>':
+                                            writer.print("&gt");
+                                            break;
+                                        case '&':
+                                            writer.print("&amp");
+                                            break;
+                                        default:
+                                            writer.print(string.charAt(i));
+                                    }
                                     i++;
                                     if (i == string.length()) {
-                                        writer.print("</br>");
+                                        writer.print("<br/>");
                                         string = scanner.nextLine();
                                         i = 0;
                                     }
                                 }
-                                if (string.charAt(i) == '"' && string.charAt(i + 1) == '"'){
+                                if (string.charAt(i) == '"' && string.charAt(i + 1) == '"') {
                                     writer.print(string.charAt(i + 1));
                                 }
                             }
-                            isInside = false;
+                            insideQuotes = false;
                         }
                     } else if (symbol == ',') {
                         writer.print("</td><td>");
                     } else {
                         while (string.charAt(i) != ',') {
-                 //           System.out.print(string.charAt(i));
-                            writer.print(string.charAt(i));
+                            switch (string.charAt(i)) {
+                                case '<':
+                                    writer.print("&lt");
+                                    break;
+                                case '>':
+                                    writer.print("&gt");
+                                    break;
+                                case '&':
+                                    writer.print("&amp");
+                                    break;
+                                default:
+                                    writer.print(string.charAt(i));
+                            }
                             if (i == string.length() - 1) {
                                 writer.print("</tr>");
                                 break;
@@ -68,12 +84,11 @@ public class NewCSV {
                             }
                             i++;
                         }
-                        //writer.print("</td>");
                     }
                 }
                 System.out.println();
             }
-            writer.println("<table>");
+            writer.println("</table>");
             writer.println("</body>");
             writer.println("</html>");
 
